4e7b0663a8d0ecb9516d2dba695543dcc26f9930
Noor Abbas <shaikhnoor1512@gmail.com>
2024-07-24 12:51:42 +0530

    CRUD Operation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'todo-app';
+  constructor() {
+    console.log('This is Console');
+  }
 }
